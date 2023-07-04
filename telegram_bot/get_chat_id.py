25d1e387ae51d8b0ded41cedfd90d77aa685ebc5
juliazubko <102211232+juliazubko@users.noreply.github.com>
2023-07-04 17:03:57 +0200

    Update get_chat_id.py

--- telegram_bot/get_chat_id.py
+++ telegram_bot/get_chat_id.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+# send at least one message first 
+
+
 url = f'https://api.telegram.org/bot{"...your bot token here..."}/getUpdates'
 response = requests.get(url)
 updates = json.loads(response.text)
