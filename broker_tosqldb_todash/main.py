4d2322bc13a143811136e37d202d8a421f0eb772
juliazubko <102211232+juliazubko@users.noreply.github.com>
2023-07-04 16:52:03 +0200

    Add files via upload

---
+++ broker_tosqldb_todash/main.py
@@ -0,0 +1,29 @@
+import time
+import threading
+from lib.mqtt_dbhandler import MQTTDatabaseHandler
+from lib.dashboard_handler import DashBoardHandler
+
+
+if __name__ == "__main__":
+    # MQTT Database Handler
+    db_handler = MQTTDatabaseHandler(broker="broker.hivemq.com", port=1883, topic="topic-test-1",
+                                     db_file="sensor_data.db", table_name="sensor_data")
+
+    # DashBoard Handler
+    dash_handler = DashBoardHandler(db_file="sensor_data.db", sensor_table_name="sensor_data")
+
+    # Create three threads
+    db_thread = threading.Thread(target=db_handler.run)
+    dash_thread = threading.Thread(target=dash_handler.run)
+
+    # Start the threads
+    db_thread.start()
+
+    # Add some delay before starting the dashboard
+    time.sleep(5)  # wait for 5 seconds
+
+    dash_thread.start()
+
+
+
+
